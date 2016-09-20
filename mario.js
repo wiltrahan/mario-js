9e92180cdafd8d4ad9296927f2af52f9bba53dda
jesseilev <jesseilev@gmail.com>
2016-09-20 18:34:09 -0500

    consolidate comments

--- mario.js
+++ mario.js
@@ -7,24 +7,23 @@ drawPyramid(5);
  *
  * Renders, in the HTML document, a Mario pyramid of the specified height
  */
-function drawPyramid(height) {
-    // height is the number of rows. For each row...
-    for (var row = 0; row < height; row++) {
-        // figure out how many bricks in this row (aka the '#' characters)
-        var numBricks = row + 2;
+ function drawPyramid(height) {
 
-        // figure out how many space characters
-        var numSpaces = height - row - 1;
+     // for each row....
+     for (var row = 0; row < height; row++) {
 
-        // build up a string for this row
-        var rowStr = "";
-        for (var i = 0; i < numSpaces; i++) {
-            rowStr += ".";
-            // we are using a period instead of a space because HTML ignores whitespace
-        }
-        for (var i = 0; i < numBricks; i++) {
-            rowStr += "#";
-        }
+         // figure out number of bricks and spaces
+         var numBricks = row + 2;
+         var numSpaces = height - row - 1;
+
+         // build up a string for this row
+         var rowStr = "";
+         for (var i = 0; i < numSpaces; i++) {
+             rowStr += ".";
+         }
+         for (var i = 0; i < numBricks; i++) {
+             rowStr += "#";
+         }
 
         // create a text element with the string of characters
         textElem = document.createTextNode(rowStr);
