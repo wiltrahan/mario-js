f6abd914aecb7b296d408fd83ac50c2682d8989e
jesseilev <jesseilev@gmail.com>
2016-09-20 18:24:42 -0500

    docstring

--- mario.js
+++ mario.js
@@ -1,17 +1,23 @@
 
 printPyramid(5);
 
+
+/*
+ * printPyramid
+ *
+ * Prints to the console a pyramid of '#' characters of the specified height
+ * For example, if height is 5, the console will look like this:
+ *          ##
+ *         ###
+ *        ####
+ *       #####
+ *      ######
+ */
 function printPyramid(height) {
     console.log("Uh oh... the pyramid is under construction.");
     console.log("Check back soon, our developers are hard at work as we speak!");
 
     // TODO
-    // print a pyramid of '#' characters to the console
-    // for example, if height is 5, the console should look like this:
-    //     ##
-    //    ###
-    //   ####
-    //  #####
-    // ######
+    // print that pyramid!
 
 }
