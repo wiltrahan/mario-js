04de4ab6e3932dd7506fba1ce7367573f95adf72
jesseilev <jesseilev@gmail.com>
2016-09-20 18:30:04 -0500

    more comments

--- mario.js
+++ mario.js
@@ -14,19 +14,25 @@ printPyramid(5);
  *      ######
  */
 function printPyramid(height) {
+
     // height is the number of rows. For each row...
     for (var row = 0; row < height; row++) {
+
         // figure out how many bricks in this row (aka the '#' characters)
         var numBricks = row + 2;
 
         // figure out how many space characters
         var numSpaces = height - row - 1;
 
-        // build up a string for this row
+        // build up a string for this row. Starts empty...
         var rowStr = "";
+
+        // add the spaces
         for (var i = 0; i < numSpaces; i++) {
             rowStr += " ";
         }
+
+        // add the bricks
         for (var i = 0; i < numBricks; i++) {
             rowStr += "#";
         }
