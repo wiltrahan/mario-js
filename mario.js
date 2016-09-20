fd0af4be3d28245dcaa48b761120a76fd45eeff6
jesseilev <jesseilev@gmail.com>
2016-09-20 18:25:32 -0500

    docstring, finish last coment

--- mario.js
+++ mario.js
@@ -1,6 +1,18 @@
 
 printPyramid(5);
 
+
+/*
+ * printPyramid
+ *
+ * Prints to the console a pyramid of '#' characters of the specified height
+ * For example, if height is 5, the console will look like this:
+ *          ##
+ *         ###
+ *        ####
+ *       #####
+ *      ######
+ */
 function printPyramid(height) {
     // height is the number of rows. For each row...
     for (var row = 0; row < height; row++) {
@@ -19,7 +31,7 @@ function printPyramid(height) {
             rowStr += "#";
         }
 
-        // print the
+        // print the string
         console.log(rowStr);
     }
 }
