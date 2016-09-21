5d53a7d2999e3fb3217aa90b9e92b6374d9b366e
jesseilev <jesseilev@gmail.com>
2016-09-21 15:59:58 -0500

    fix indentation

--- mario.js
+++ mario.js
@@ -56,27 +56,27 @@ function clearError(message) {
  *
  * Renders, in the HTML document, a Mario pyramid of the specified height
  */
- function drawPyramid(height) {
+function drawPyramid(height) {
 
-     // first, clear the old content
-     document.getElementById("pyramid").innerHTML = "";
+    // first, clear the old content
+    document.getElementById("pyramid").innerHTML = "";
 
-     // for each row....
-     for (var row = 0; row < height; row++) {
+    // for each row....
+    for (var row = 0; row < height; row++) {
 
-         // figure out number of bricks and spaces
-         var numBricks = row + 2;
-         var numSpaces = height - row - 1;
+        // figure out number of bricks and spaces
+        var numBricks = row + 2;
+        var numSpaces = height - row - 1;
 
-         // build up a string for this row
-         var rowStr = "";
-         for (var i = 0; i < numSpaces; i++) {
-             var spaceChar = "&nbsp";
-             rowStr += spaceChar;
-         }
-         for (var i = 0; i < numBricks; i++) {
-             rowStr += "#";
-         }
+        // build up a string for this row
+        var rowStr = "";
+        for (var i = 0; i < numSpaces; i++) {
+         var spaceChar = "&nbsp";
+         rowStr += spaceChar;
+        }
+        for (var i = 0; i < numBricks; i++) {
+         rowStr += "#";
+        }
 
         // create a text element with the string of characters
         textElem = document.createTextNode(rowStr);
