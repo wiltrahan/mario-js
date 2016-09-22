991d824e1f668fc475875fde362a87e6e3bfc565
jesseilev <jesseilev@gmail.com>
2016-09-22 16:13:27 -0500

    fix indentation

--- mario.js
+++ mario.js
@@ -2,35 +2,34 @@
 drawPyramid(5);
 
 
-/**
- * drawPyramid
- *
- * Renders, in the HTML document, a Mario pyramid of the specified height
- */
- function drawPyramid(height) {
+    /**
+    * drawPyramid
+    *
+    * Renders, in the HTML document, a Mario pyramid of the specified height
+    */
+    function drawPyramid(height) {
 
-     // for each row....
-     for (var row = 0; row < height; row++) {
+        // for each row....
+        for (var row = 0; row < height; row++) {
 
-         // figure out number of bricks and spaces
-         var numBricks = row + 2;
-         var numSpaces = height - row - 1;
+        // figure out number of bricks and spaces
+        var numBricks = row + 2;
+        var numSpaces = height - row - 1;
 
-         // build up a string for this row
-         var rowStr = "";
-         for (var i = 0; i < numSpaces; i++) {
-             rowStr += ".";
-         }
-         for (var i = 0; i < numBricks; i++) {
-             rowStr += "#";
-         }
+        // build up a string for this row
+        var rowStr = "";
+        for (var i = 0; i < numSpaces; i++) {
+            rowStr += ".";
+        }
+        for (var i = 0; i < numBricks; i++) {
+            rowStr += "#";
+        }
 
-        // create a text element with the string of characters
-        textElem = document.createTextNode(rowStr);
-
-        // create a <p> element with the text inside
+        // create a <p> element
         rowElem = document.createElement("p");
-        rowElem.appendChild(textElem);
+
+        // set rowStr to be the inner text of the <p>
+        rowElem.innerHTML = rowStr;
 
         // insert the paragraph as a child of the <body>
         document.body.appendChild(rowElem);
